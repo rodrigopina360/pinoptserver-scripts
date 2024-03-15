638ebd1f124cebe1e28a356a2c3587d69f8a5d51
Rodrigo Pina <rodrigooliveirapina@gmail.com>
2024-03-15 03:58:00 +0000

    Created new prod branch until I can make dev and prod guweb use the same python

--- start/guweb.sh
+++ start/guweb.sh
@@ -3,7 +3,7 @@
 workingDirectory="/home/pinossaur/pinoptserver/scripts/"
 pythonDirectory="/home/pinossaur/pinoptserver/guweb/"
 
-nohup python3 "$pythonDirectory"main.py > /dev/null 2>&1 &
+nohup python3.9 "$pythonDirectory"main.py > /dev/null 2>&1 &
 
 cd $workingDirectory
 ./wait-for-it.sh -h localhost -p 8000 -s -t 3 -q
